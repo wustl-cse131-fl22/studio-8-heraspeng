ac90a4b53c72c1575051aa1e92dea76904c6a329
johnli <johnli@johns-mbp.dhcp.wustl.edu>
2022-11-17 13:57:09 -0600

    i did my part

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,61 @@
 package studio8;
 
+import java.util.Objects;
+
 public class Date {
 
-
-    public static void main(String[] args) {
-
+	private int YEAR;
+	private int MONTH;
+	private int DAYS;
+	private boolean isHoli;
+	
+	/**
+	 * 
+	 */
+	public Date() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * create a Date
+	 * 
+	 * @param year
+	 * @param month
+	 * @param days
+	 */
+	public Date(int year, int month, int days, boolean i) {
+		YEAR = year;
+		MONTH = month;
+		DAYS = days;	
+		isHoli = i;
+	}
+	
+	public String dateToString () {
+		String result = "";
+		if (isHoli) {
+			result = "Date is: " + MONTH + "/" + DAYS + "/" + YEAR + " Today is a holiday!";
+		} else if (!isHoli) {
+			result = "Date is: " + MONTH + "/" + DAYS + "/" + YEAR;
+		}
+		return result;
+	}
+    @Override
+	public int hashCode() {
+		return Objects.hash(DAYS, MONTH, YEAR);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return DAYS == other.DAYS && MONTH == other.MONTH && YEAR == other.YEAR;
+	}
+	public static void main(String[] args) {
+		Date nDate = new Date(2003,9,24,true);
+		System.out.println(nDate.dateToString());
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,59 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
 
+	private int hour;
+	private int min;
+	private int sec;
+	private boolean isTwentyFour; 
+
+	public Time() {
+		
+	}
+	
+	public Time (int h, int m, int s, boolean i) {
+		hour = h;
+		min = m;
+		sec = s;
+		isTwentyFour = i;
+	}
 	public static void main(String[] args) {
- 
+		Time nTime = new Time(15,2,57,true);
+		System.out.println(nTime.timeToString());
     	
     }
+	public String timeToString() {
+		String result = "";
+		if (isTwentyFour && hour > 12) {
+			int h = (hour - 12);
+			result = "Time is: " + h + ":" + min + ":" + sec + "PM";
+		} else if (isTwentyFour && hour < 12) {
+			result = "Time is: " + hour + ":" + min + ":" + sec + "AM";
+		} else if (isTwentyFour && hour == 12) {
+			result = "Time is: " + hour + ":" + min + ":" + sec + "PM";
+		} if (hour == 0) {
+			result = "Time is: " + 12 + ":" + min + ":" + sec + "AM";
+		} else if (!isTwentyFour) {
+			result = "Time is: " + hour + ":" + min + ":" + sec;
+		}
+		return result;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, min, sec);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && min == other.min && sec == other.sec;
+	}
 
 }
